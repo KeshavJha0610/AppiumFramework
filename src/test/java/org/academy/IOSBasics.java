a734a7c06b3dfc0b28be08eb7ae1f183bb3b899c
keshav.jha <keshav.jha@medibuddy.in>
2022-07-17 13:43:49 +0530

    Added New Code

---
+++ src/test/java/org/academy/IOSBasics.java
@@ -0,0 +1,29 @@
+package org.academy;
+
+import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
+import org.academy.TestUtils.IOSBaseTest;
+import org.academy.pageObjects.ios.AlertViews;
+
+public class IOSBasics extends IOSBaseTest{
+
+	
+	@Test
+	public void IOSBasicsTest()
+	{
+		//Xpath, classname, IOS, iosClassCHain, IOSPredicateString, accessibility id, id
+		
+		AlertViews alertViews = homePage.selectAlertViews();
+		alertViews.fillTextLabel("hello");
+		String actualMessage = alertViews.getConfirmMessage();
+		AssertJUnit.assertEquals(actualMessage, "A message should be a short, complete sentence.");
+
+		
+		
+		
+		
+		
+		
+		
+	}
+}
